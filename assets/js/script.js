e9cefbc2d5359f5db6c6e81c62693fb00aa1df6e
Павел Короленко <pavlecorolenko535@gmail.com>
2021-02-09 22:25:50 +0200

    +second screen

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,6 +13,16 @@ $(document).ready(function () {
 
   const menuBtnBurger = document.querySelector(".menu-btn__burger");
 
+  $(".tabs li a").click(function() {
+		
+		$(".tabs li a").removeClass("active");
+		$(this).addClass("active");
+		
+		$(".tab_content_container > .tab_content_active").removeClass("tab_content_active").fadeOut(200);
+		$(this.rel).fadeIn(500).addClass("tab_content_active");
+		
+	});	
+
 // Sliders
   
   // 1
@@ -33,5 +43,26 @@ $(document).ready(function () {
     nav:true,
     responsiveClass:true,
     items:9,
+  });
+
+  // news 
+
+  $(".sliderNews").owlCarousel({
+    loop:true,
+    margin:25,
+    nav:false,
+    responsiveClass:true,
+    items:4,
+  });
+
+  // footer 
+
+  $("#footerSlide").owlCarousel({
+    loop:true,
+    margin:-50,
+    navText: [],
+    nav:true,
+    responsiveClass:true,
+    items:4,
   });
 });
